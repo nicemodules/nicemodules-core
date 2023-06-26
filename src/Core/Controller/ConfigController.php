35f8cf014b03341cdd6341daeb7aa7072c59089c
jedrek <nicemodules@gmail.com>
2023-06-26 23:02:04 +0200

    added ConfigController

---
+++ src/Core/Controller/ConfigController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace NiceModules\Core\Controller;
+
+use NiceModules\Core\Controller;
+
+class ConfigController extends Controller
+{
+
+    public function render(){
+        
+    }
+    
+    public function getData(){
+        
+    }
+    
+    public function edit(){
+        
+    }
+
+}
